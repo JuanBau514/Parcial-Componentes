c980c49694157a3f0fae270b01a18c7ac4202c0c
Jimmy Saavedra <jim312002@hotmail.com>
2024-03-30 23:40:31 -0500

    Funcionalidad de tambor

--- app/src/main/java/com/example/parcial/logic/Maraca.kt
+++ app/src/main/java/com/example/parcial/logic/Maraca.kt
@@ -1,6 +1,9 @@
 package com.example.parcial.logic
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +15,7 @@ import com.example.parcial.ui.theme.MainActivity
 
 class Maraca : AppCompatActivity() {
     private lateinit var binding: ActivityMaracaBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/parcial/logic/Tambor.kt
+++ app/src/main/java/com/example/parcial/logic/Tambor.kt
@@ -7,19 +7,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.parcial.R
-import com.example.parcial.databinding.ActivityRulesBinding
 import com.example.parcial.databinding.ActivityTamborBinding
 import com.example.parcial.ui.theme.MainActivity
+import android.media.MediaPlayer
 
 class Tambor : AppCompatActivity() {
 
     private lateinit var binding: ActivityTamborBinding
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding= ActivityTamborBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.tambor)
+
+        binding.btnPlayTambor.setOnClickListener {
+            mediaPlayer.seekTo(100)
+
+            mediaPlayer.start()
+        }
 
         binding.btnCancelTambor.setOnClickListener {
             var intent = Intent(this, MainActivity::class.java)
