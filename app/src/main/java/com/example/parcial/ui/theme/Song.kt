d08ab4e8541e6f91b6e2bbdca04e710c42e18090
juan bautista <juanbautista@MacBook-Pro-de-juan.local>
2024-03-31 00:07:28 -0500

    mostrando la informacion de las canciones error al mostrar las lyrics

--- app/src/main/java/com/example/parcial/ui/theme/Song.kt
+++ app/src/main/java/com/example/parcial/ui/theme/Song.kt
@@ -1,6 +1,7 @@
 package com.example.parcial.ui.theme
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +11,24 @@ import com.example.parcial.databinding.ActivitySongBinding
 
 class Song : AppCompatActivity() {
 
+
     private lateinit var binding: ActivitySongBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Recuperar los datos de la canción del Intent
+        val songName = intent.getStringExtra("songName")
+        val songLyrics = intent.getStringExtra("songLyrics")
+
+        // Obtener las referencias de los TextView
+        val mostrarNombre = findViewById<TextView>(R.id.txtTitleSong)
+        val mostrarLetra = findViewById<TextView>(R.id.txtInputEditSong)
+
+        // Establecer el texto en los TextView
+        mostrarNombre.text = songName
+        mostrarLetra.text = songLyrics
     }
 }
